df2bc4d796b7c35880e2f4210ceeb979ab281669
psyos9 <psyos9@exmail.nottingham.ac.uk>
2025-03-08 18:21:13 +0000

    refactor

--- camera_test.py
+++ camera_test.py
@@ -1,9 +1,10 @@
+import subprocess
 from time import sleep
 
 input("this script takes photos, press enter to continue")
 
 def take_photo():
-    exec("libcamera-still -o img.jpg")
+    subprocess.run(["libcamera-still","-o","img.jpg"])
     sleep(1)
 
 while True:
