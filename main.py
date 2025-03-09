960e451e019759736687b11f71e6da0858ebd667
Max Carmona <psymc11@nottingham.ac.uk>
2025-03-09 09:38:10 +0000

    thumbs up check for plant scanning

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import hand_detection
 import subprocess
 from time import sleep
 
+plantScan = False
+
 startup()
 while True:
 	subprocess.run(["libcamera-still","-o","assets/gregors_view.png"])
-	action = hand_detection.detect_action("assets/gregors_view.png")
-	print(action)
-	if action: play(action)
 
-#identify_plant("assets/gregors_view.png")
+	if plantScan:
+		identify_plant("assets/gregors_view.png")
+		plantScan = False
+	else:
+		action = hand_detection.detect_action("assets/gregors_view.png")
+		print(action)
+		if action == "thumbs up":
+			play("scanning for plants")
+			plantScan = True
+
+
