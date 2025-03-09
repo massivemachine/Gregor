961173d2dad37d85d6deaaed66f3b2ab4e63039a
Max Carmona <psymc11@nottingham.ac.uk>
2025-03-09 09:13:36 +0000

    fixed loop

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from time import sleep
 startup()
 while True:
 	subprocess.run(["libcamera-still","-o","assets/gregors_view.png"])
-
-action = hand_detection.detect_action("assets/gregors_view.png")
-print(action)
-if action: play(action)
+	action = hand_detection.detect_action("assets/gregors_view.png")
+	print(action)
+	if action: play(action)
 
 #identify_plant("assets/gregors_view.png")
