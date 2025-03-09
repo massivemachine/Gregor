78d99a67016cf7ffe367d2782151f243e2bb429a
Max Carmona <psymc11@nottingham.ac.uk>
2025-03-09 01:39:56 +0000

    hand sounds

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from plant_detection import *
+import hand_detection
 import subprocess
 from time import sleep
 
 startup()
 subprocess.run(["libcamera-still","-o","assets/gregors_view.png"])
+
+play(hand_detection.detect_action("gregors_view.png"))
 
 identify_plant("assets/gregors_view.png")
